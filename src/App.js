9c726e2809116e3117b1c72307a4471a19d2c376
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-19 23:59:06 +0600

    Showed thsirt data from home

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const router = createBrowserRouter([
     children:[
       {
         path: '/',
+        loader: () => fetch('tshirts.json'),
         element: <Home></Home>
       },
       {
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import Tshirt from '../Tshirt/Tshirt';
+import ('./Home.css');
 
 const Home = () => {
+    const tshirts = useLoaderData()
+    console.log(tshirts)
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='home-container'>
+            <div className='t-shirt-container'>
+            {
+                tshirts.map(tshirt => <Tshirt tshirt={tshirt}></Tshirt>)
+            }
+            </div>
+            <div className='cart-container'>
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
